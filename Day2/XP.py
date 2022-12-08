b3cba7df0d94700a97769e576ee8e1a09d37b3f5
NasserODG <nasserodg60@gmail.com>
2022-12-08 09:28:03 +0000

    Day 2 exo

--- Day2/XP.py
+++ Day2/XP.py
@@ -1,4 +1,4 @@
-        # EXERCICE 1
+print("EXERCICE 1")
 
 my_fav_numbers = set()
 
@@ -19,13 +19,15 @@ print(friend_fav_numbers)
 
 our_fav_numbers = my_fav_numbers.union(friend_fav_numbers)
 print(our_fav_numbers)
+print("\n\n")
 
 
-        #EXERCICE 2
+print("EXERCICE 2")
 
 print("Non ceci n'est pas possible car les tuples sont immuables")
+print("\n\n")
 
-        #EXERCICE 3
+print("EXERCICE 3")
 
 basket = ["Banana", "Apples", "Oranges", "Blueberries"]
 
@@ -39,3 +41,41 @@ print(nbr)
 
 basket.clear()
 print(basket)
+
+print("\n\n")
+
+
+print("EXERCICE 5")
+
+for x in range(21):
+    print(x)
+
+for x in range(0, 21, 2):
+    print(x)
+
+
+print("\n\n")
+
+print("EXERCICE 6")
+
+x = "Nasser"
+while True:
+    y = input("Entrer un  nom correspondant à mon nom :")
+    if y == x:
+        print("Vous avez réussis !!!")
+        break
+    else:
+        y = input("Entrer un  nom correspondant à mon nom :")
+print("\n\n")
+
+print("EXERCICE 7")
+
+fruit_pref = []
+fruit_pref = input("Entrez la liste de vos fruits préférés en laissant seulement des espaces entre eux sur la même ligne :")
+
+fruit = []
+fruit = input("Entrez maintenant le nom d'un fruit :")
+if fruit in fruit_pref :
+    print("Vous avez choisi l'un de vos fruits préférés ! Prendre plaisir!")
+else :
+    print("Vous avez choisi un nouveau fruit. J'espère que vous appréciez")
